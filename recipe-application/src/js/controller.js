87c738b4385a6130434b0cc499842cb0b5f1fc6b
CharlotteImenkamp <charlotte.imenkamp@gmail.com>
2024-02-28 13:32:29 +0100

    load recipe

--- recipe-application/src/js/controller.js
+++ recipe-application/src/js/controller.js
@@ -7,3 +7,35 @@ const timeout = function (s) {
     }, s * 1000);
   });
 };
+
+const showRecipe = async function () {
+  try {
+    const res = await fetch(
+      //'https://forkify-api.herokuapp.com/api/get?rId=47746'
+      'https://forkify-api.herokuapp.com/api/get?rId=46956'
+    );
+    const data = await res.json(); 
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+    
+    console.log(data); 
+
+    // refactor data
+    let { recipe } = data; 
+    recipe = {
+      id: recipe.recipe_id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      ingredients: recipe.ingredients,
+    } 
+
+    console.log(recipe); 
+
+  } catch (err) {
+    console.error(err); 
+  }
+}
+
+showRecipe(); 
+console.log('hello js'); 
