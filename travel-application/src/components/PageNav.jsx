5ce0eec8d246bf0610a0d9b1248b048cd83b7e39
CharlotteImenkamp <charlotte.imenkamp@gmail.com>
2024-03-05 15:38:11 +0100

    add map, spinner and sidebar components

--- travel-application/src/components/PageNav.jsx
+++ travel-application/src/components/PageNav.jsx
@@ -1,10 +1,12 @@
 import { NavLink } from "react-router-dom";
+import Logo from "./Logo"; 
 import styles from "./PageNav.module.css"; 
 
 
 function PageNav() {
   return (
     <nav className={styles.nav}>
+      <Logo/>
       <ul>
         <li>
           <NavLink to="/">Home</NavLink>
@@ -14,6 +16,9 @@ function PageNav() {
         </li>
         <li>
           <NavLink to="/product">Product</NavLink>
+        </li>
+        <li>
+          <NavLink to="/login" className={styles.ctaLink}>Login</NavLink>
         </li>
       </ul>
     </nav>
