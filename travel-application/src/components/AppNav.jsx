344c64d7232e885bb4b2577f64dd98824a3d702a
CharlotteImenkamp <charlotte.imenkamp@gmail.com>
2024-03-05 16:39:15 +0100

    add nested routing in sidebar

--- travel-application/src/components/AppNav.jsx
+++ travel-application/src/components/AppNav.jsx
@@ -1,11 +1,18 @@
-
 import styles from "./AppNav.module.css";
+import { NavLink } from "react-router-dom";
 
 function AppNav() {
   return (
-    <div className={styles.nav}>
-      appnav
-    </div>
+    <nav className={styles.nav}>
+      <ul>
+        <li>
+          <NavLink to="cities">Cities</NavLink>
+        </li>
+        <li>
+          <NavLink to="countries">Countries</NavLink>
+        </li>
+      </ul>
+    </nav>
   );
 }
 
