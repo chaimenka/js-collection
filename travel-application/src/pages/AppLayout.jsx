09116d675def27f2ee8ef4fc613ac09d7d00d4ed
CharlotteImenkamp <charlotte.imenkamp@gmail.com>
2024-03-05 16:28:33 +0100

    add sidebar and map files

--- travel-application/src/pages/AppLayout.jsx
+++ travel-application/src/pages/AppLayout.jsx
@@ -1,10 +1,12 @@
-import AppNav from "../components/AppNav";
+import Sidebar from "../components/Sidebar";
+import Map from "../components/Map"; 
+import styles from './AppLayout.module.css'
 
 function AppLayout() {
   return (
-    <div >
-      <AppNav />
-      <div>app</div>
+    <div className={styles.app}>
+      <Sidebar />
+      <Map/>
     </div>
   );
 }
