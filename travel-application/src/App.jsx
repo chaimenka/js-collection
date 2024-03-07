89e1b1bf96265faa26d1910efd8165c55642e247
CharlotteImenkamp <charlotte.imenkamp@gmail.com>
2024-03-07 13:30:34 +0100

    add protected route

--- travel-application/src/App.jsx
+++ travel-application/src/App.jsx
@@ -13,6 +13,7 @@ import CountryList from "./components/CountryList";
 import Form from "./components/Form";
 import { CitiesProvider } from "./contexts/CitiesContext";
 import { AuthProvider } from "./contexts/FakeAuthContext";
+import ProtectedRoute from "./pages/ProtectedRoute";
 
 const BASE_URL = 'http://localhost:8000'; 
 
@@ -28,7 +29,14 @@ function App() {
             <Route path="pricing" element={<Pricing />} />
             <Route path="product" element={<Product />} />
             <Route path="login" element={<Login />} />
-            <Route path="app" element={<AppLayout />}>
+            <Route
+              path="app"
+              element={
+                <ProtectedRoute>
+                  <AppLayout />
+                </ProtectedRoute>
+              }
+            >
               {/* /** replace last page in history stack. normally only used for this usecase. navigate hook otherwise */}
               <Route index element={<Navigate replace to="cities" />} />
               <Route path="cities" element={<CityList />} />
