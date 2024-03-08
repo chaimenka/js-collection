6174ed9b5cde94f95dceb819e2fcd52631f130d4
CharlotteImenkamp <charlotte.imenkamp@gmail.com>
2024-03-08 15:10:54 +0100

    add browser router to dummy pages

--- fast-pizza/src/App.jsx
+++ fast-pizza/src/App.jsx
@@ -1,6 +1,24 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom"; 
+import Home from "./ui/Home"; 
+import Menu from "./features/menu/Menu"; 
+import Cart from "./features/cart/Cart"; 
+import CreateOrder from "./features/order/CreateOrder"; 
+import Order from "./features/order/Order"; 
+
+// npm i react-router-dom
+const router = createBrowserRouter([
+  { path: "/", element: <Home />,},
+  { path: "/menu", element: <Menu />,},
+  { path: "/cart", element: <Cart />,},
+  { path: "/order/new", element: <CreateOrder />,},
+  { path: "/order/:orderId", element: <Order />,},
+]); 
+
 
 function App() {
-  return <div>Hello Vite!</div>
+  return <RouterProvider router={router}>
+    
+  </RouterProvider>; 
 }
 
 export default App
