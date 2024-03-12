1ba50f3229334735dc87fb398c8620026a9a6cd4
CharlotteImenkamp <charlotte.imenkamp@gmail.com>
2024-03-12 12:41:14 +0100

    style cart

--- fast-pizza/src/features/cart/Cart.jsx
+++ fast-pizza/src/features/cart/Cart.jsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 import LinkButton from "../../ui/LinkButton"
 import Button from "../../ui/Button"
+import CartItem from './CartItem';
 
 
 const fakeCart = [
@@ -35,10 +36,16 @@ function Cart() {
       <LinkButton to="/menu">&larr; Back to menu</LinkButton>
 
       <h2>Your cart, %NAME%</h2>
+      <ul className="mt-3 divide-y divide-zinc-200 border-b">
 
-      <div>
+        {cart.map((item) => (
+          <CartItem item={item} key={item.key} />
+        ))}
+      </ul>
+
+      <div className="mt-6 space-x-2">
         <Button type ="primary" to="/order/new">Order pizzas</Button>
-        <button>Clear cart</button>
+        <Button type ="secondary">Clear cart</Button>
       </div>
     </div>
   );
