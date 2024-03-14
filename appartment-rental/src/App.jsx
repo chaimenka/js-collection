fd1b135e030f995ec91285edf734b7ee1f867ad4
CharlotteImenkamp <charlotte.imenkamp@gmail.com>
2024-03-14 14:57:41 +0100

    rental: add routes

--- appartment-rental/src/App.jsx
+++ appartment-rental/src/App.jsx
@@ -1,9 +1,22 @@
 import styled from "styled-components";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+
+
 import GlobalStyles from "./styles/GlobalStyles";
 import Button from "./ui/Button";
 import Input from "./ui/Input";
 import Heading from "./ui/Heading";
 import Row from "./ui/Row";
+
+import Dashboard from "./pages/Dashboard";
+import Bookings from "./pages/Bookings";
+import Cabins from "./pages/Cabins";
+import Users from "./pages/Users";
+import Settings from "./pages/Settings";
+import Account from "./pages/Account";
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./ui/AppLayout";
 
 const StyledApp = styled.main`
   padding: 20px;
@@ -13,28 +26,22 @@ function App() {
   return (
     <>
       <GlobalStyles />
-      <StyledApp>
-        <Row type="horizontal">
-          <Heading as="h1">Appartment Rental</Heading>
-          <div>
-            <Heading as="h2">Check in and out</Heading>
-            <Button variation="primary" size="medium" onClick={() => {}}>
-              Check In
-            </Button>
-            <Button variation="secondary" size="small" onClick={() => {}}>
-              Check Out
-            </Button>
-          </div>
-        </Row>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to="dashboard" />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="bookings" element={<Bookings />} />
+            <Route path="cabins" element={<Cabins />} />
+            <Route path="users" element={<Users />} />
+            <Route path="settings" element={<Settings />} />
+            <Route path="account" element={<Account />} />
+          </Route>
 
-        <Row type="vertical">
-          <form>
-            <Heading as="h3">Form</Heading>
-            <Input type="number" placeholder="Number of Guests" />
-            <Input type="number" placeholder="Number of Guests" />
-          </form>
-        </Row>
-      </StyledApp>
+          <Route path="login" element={<Login />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
