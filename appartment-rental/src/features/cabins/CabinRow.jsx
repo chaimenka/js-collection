ed61d9d95687d0c3d664c121da76a6474d804a35
CharlotteImenkamp <charlotte.imenkamp@gmail.com>
2024-04-04 15:58:01 +0200

    rental: change table to compound component

--- appartment-rental/src/features/cabins/CabinRow.jsx
+++ appartment-rental/src/features/cabins/CabinRow.jsx
@@ -9,18 +9,8 @@ import { HiSquare2Stack } from "react-icons/hi2";
 import { useCreateCabin } from "./useCreateCabin";
 import Modal from "../../ui/Modal";
 import ConfirmDelete from "../../ui/ConfirmDelete";
+import Table from "../../ui/Table";
 
-const TableRow = styled.div`
-  display: grid;
-  grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
-  column-gap: 2.4rem;
-  align-items: center;
-  padding: 1.4rem 2.4rem;
-
-  &:not(:last-child) {
-    border-bottom: 1px solid var(--color-grey-100);
-  }
-`;
 
 const Img = styled.img`
   display: block;
@@ -73,7 +63,7 @@ function CabinRow({ cabin }) {
   }
 
   return (
-    <TableRow rol="row">
+    <Table.Row rol="row">
       <Img src={image} />
       <Cabin>{name}</Cabin>
       <div> Fits up to {maxCapacity}</div>
@@ -109,7 +99,7 @@ function CabinRow({ cabin }) {
           </Modal.Window>
         </Modal>
       </div>
-    </TableRow>
+    </Table.Row>
   );
 }
 
